fc1cd78c78fbdb2e23b1aa09ae0abc7ccfb5702e
luqman-bashir <uthamin98@gmail.com>
2024-10-05 22:21:46 +0300

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const getElement=document.getElementById("main")
+if(getElement){
+    getElement.remove()
+}
+ const newHeader=document.createElement("h1")
+
+ newHeader.id="victory"
+
+ const yourName = 'Luqman Bshir'; 
+ newHeader.textContent = `${yourName} is the champion`;  
+
+
+ document.body.appendChild(newHeader);  
